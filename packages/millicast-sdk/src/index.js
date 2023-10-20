e6dd7f9dcbca4461355168b872d60bf078f3678c
Jayson DeLancey <jayson.delancey@dolby.com>
2023-10-19 17:00:55 -0700

    find package version with millicast.version

--- packages/millicast-sdk/src/index.js
+++ packages/millicast-sdk/src/index.js
@@ -5,11 +5,16 @@ import Director from './Director'
 import Publish from './Publish'
 import View from './View'
 
+// TODO: pull this during build from package.json
+// const version = '0.1.41';
+const version = '@npm_package_version'
+
 export {
   Logger,
   PeerConnection,
   Signaling,
   Director,
   Publish,
-  View
+  View,
+  version
 }
